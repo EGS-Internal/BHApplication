039785e41fb42891ee1ee5d235fd2452e8bf1c51
hoang.dd <hoang.dd@eastgate-software.com>
2024-03-21 11:43:44 +0700

    configure datacontext and connection string

---
+++ BHGroup.DAL/DbContext.cs
@@ -0,0 +1,31 @@
+﻿using BHGroup.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Configuration;
+
+namespace BHGroup.DAL
+{
+    internal class DBContext : DbContext
+    {
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Lecturer> Lecturers { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>().HasKey(p => p.Id);
+            modelBuilder.Entity<Student>().Property(p => p.Id).ValueGeneratedOnAdd();
+            modelBuilder.Entity<Lecturer>().HasKey(p => p.Id);
+            modelBuilder.Entity<Lecturer>().Property(p => p.Id).ValueGeneratedOnAdd();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=DESKTOP-28NL6SO\\SQLEXPRESS;Database=SchoolDB;User Id=sa;Password=password;TrustServerCertificate=true");
+            }
+        }
+    }
+}
