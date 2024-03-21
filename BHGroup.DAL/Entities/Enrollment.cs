2d1e6a00027f03667231ba027ca74cb5daacf6a2
hoang.dd <hoang.dd@eastgate-software.com>
2024-03-21 14:04:10 +0700

    Add Enrollment Enitity and modify entities' relationship

---
+++ BHGroup.DAL/Entities/Enrollment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BHGroup.DAL.Entities
+{
+    public enum Grade {A,B,C,D,F };
+    public class Enrollment
+    {
+        [Required]
+        public Student Student { get; set; }
+        [Required]
+        public Course Course { get; set; }
+        [AllowNull]
+        public Grade Grade { get; set; }
+    }
+}
