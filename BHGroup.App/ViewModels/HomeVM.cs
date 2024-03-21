f08f52b8dafa5b7f2d84bae773e7ee0a96fa46b6
BinhVH <binh.vh@eastgate-software.com>
2024-03-21 09:49:12 +0700

    Add simple layout with Home and Sidebar  Navigator

---
+++ BHGroup.App/ViewModels/HomeVM.cs
@@ -0,0 +1,16 @@
+﻿using BHGroup.App.Public.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BHGroup.App.ViewModels
+{
+    class HomeVM : ObservableObject
+    {
+        public HomeVM() { 
+
+        } 
+    }
+}
---
+++ BHGroup.App/ViewModels/MainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using BHGroup.App.Public.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BHGroup.App.ViewModels
+{
+    class MainViewModel : ObservableObject
+    {
+        private object _currentView;
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { 
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+        public HomeVM HomeVM { get; set; }
+
+        public MainViewModel()
+        {
+            HomeVM = new HomeVM();
+            CurrentView = HomeVM;
+        }
+    }
+}
