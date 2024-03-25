292f3ad59a94418b9e2c234246e9c22b627f6c44
hoang.dd <hoang.dd@eastgate-software.com>
2024-03-25 11:37:16 +0700

    complete BL's simple method

---
+++ BHGroup.App/Public/Core/DIHelper.cs
@@ -0,0 +1,45 @@
+﻿using BHGroup.App.ViewModels;
+using BHGroup.BL;
+using BHGroup.DAL;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BHGroup.App.Public.Core
+{
+    class DIHelper
+    {
+        public IServiceProvider Services { get; }
+        private static DIHelper dIHelper;
+        public static DIHelper Get()
+        {
+            dIHelper ??= new DIHelper();
+            return dIHelper;
+        }
+        public DIHelper() {
+            Services = ConfigureServices();
+        }
+        private static IServiceProvider ConfigureServices()
+        {
+            var services = new ServiceCollection();
+
+            services.AddSingleton<IStudent, BLStudent>();
+            services.AddSingleton<ICourse, BLCourse>();
+            services.AddSingleton<ILecturer, BLLecturer>();
+            services.AddTransient<MainViewModel, MainViewModel>();
+            services.AddTransient<HomeVM, HomeVM>();
+            services.AddTransient<StudentVM, StudentVM>();
+            services.AddTransient<LecturerVM, LecturerVM>();
+            services.AddTransient<CourseVM, CourseVM>();
+            services.AddTransient<ClassVM, ClassVM>();
+            services.AddSingleton<DbContext, DBContext>();
+
+            return services.BuildServiceProvider();
+        }
+    }
+}
