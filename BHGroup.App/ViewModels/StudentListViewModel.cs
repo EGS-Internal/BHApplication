e3b9adafda7d7766407aac47cdb4c8f0a56465d3
BinhVH <binh.vh@eastgate-software.com>
2024-03-27 13:36:32 +0700

    complete split studentAddEdit viewmodel from list view model

--- BHGroup.App/ViewModels/StudentListViewModel.cs
+++ BHGroup.App/ViewModels/StudentListViewModel.cs
@@ -4,29 +4,29 @@ using BHGroup.App.Views.StudentWindow;
 using BHGroup.BL;
 using BHGroup.DAL.Entities;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Web.WebView2.Core;
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Drawing.Printing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Media.Animation;
 
 namespace BHGroup.App.ViewModels
 {
     class StudentListViewModel : ObservableObject
     {
+        #region Data context & repositories
         private readonly IStudent _studentContext;
-        private AddStudentView _addStudentWindow { get; set; }
+        #endregion
 
+        #region Binding properties
         private List<StudentModel> _students { get; set; }
         public List<StudentModel> Students
         {
-            get { return _students; }
-            set { _students = value; OnPropertyChanged(); }
+            get
+            {
+                return _students;
+            }
+            set
+            {
+                _students = value; 
+                OnPropertyChanged();
+            }
         }
 
         private StudentModel _selectedItem { get; set; }
@@ -36,7 +36,8 @@ namespace BHGroup.App.ViewModels
             {
                 return _selectedItem;
             }
-            set {
+            set
+            {
                 _selectedItem = value;
                 OnPropertyChanged();
                 DeleteStudentCommand.OnCanExecuteChanged();
@@ -44,16 +45,12 @@ namespace BHGroup.App.ViewModels
             }
         }
 
-        private StudentModel _studentToAdd { get; set; }
-        public StudentModel StudentToAdd {
-            get { return _studentToAdd; }
-            set { _studentToAdd = value; OnPropertyChanged(); }
-        }
-
         private bool _isButtonEnabled;
         public bool IsButtonEnabled
         {
-            get { return _isButtonEnabled; }
+            get { 
+                return _isButtonEnabled; 
+            }
             set
             {
                 if (_isButtonEnabled != value)
@@ -63,11 +60,13 @@ namespace BHGroup.App.ViewModels
                 }
             }
         }
+        #endregion
 
-        public RelayCommand OpenAddStudentViewCommand {  get; private set; }
-        public RelayCommand DeleteStudentCommand {  get; private set; }
+        #region Commands
+        public RelayCommand OpenAddStudentViewCommand { get; private set; }
+        public RelayCommand DeleteStudentCommand { get; private set; }
         public RelayCommand EditStudentCommand { get; private set; }
-        public RelayCommand AddStudentCommand { get; private set; }
+        #endregion
 
         public StudentListViewModel()
         {
@@ -76,32 +75,18 @@ namespace BHGroup.App.ViewModels
             OpenAddStudentViewCommand = new RelayCommand(ExecuteOpenAddStudentWindowCommand, CanExecuteOpenAddStudentWindowCommand);
             DeleteStudentCommand = new RelayCommand(ExecuteDeleteStudentCommand, CanExecuteDeleteStudentCommand);
             EditStudentCommand = new RelayCommand(ExecuteEditStudentCommand, CanExecuteEditStudentCommand);
-            AddStudentCommand = new RelayCommand(ExecuteAddStudentCommand, CanExecuteAddStudentCommand);
-            StudentToAdd = new StudentModel();
         }
 
+        #region Command Events
         private bool CanExecuteOpenAddStudentWindowCommand(object parameters)
         {
             return true;
         }
         private void ExecuteOpenAddStudentWindowCommand(object parameters)
         {
-            _addStudentWindow = new AddStudentView();
+            var _addStudentWindow = new AddStudentView();
             var result = _addStudentWindow.ShowDialog();
-            Students = _studentContext.GetAll().Select(s =>
-            {
-                return new StudentModel()
-                {
-                    FirstName = s.FirstName,
-                    LastName = s.LastName,
-                    StudentCode = s.StudentCode,
-                    DateOfBirth = s.DateOfBirth,
-                    Gender = s.Gender,
-                    JoinDate = s.JoinDate,
-                    Status = s.Status
-                };
-            }).ToList();
-
+            Students = _studentContext.GetAll().Select(s => new StudentModel(s)).ToList();
         }
 
         private bool CanExecuteDeleteStudentCommand(object parameters)
@@ -114,13 +99,13 @@ namespace BHGroup.App.ViewModels
             else
             {
                 IsButtonEnabled = false;
-                return false; 
+                return false;
             }
         }
         private void ExecuteDeleteStudentCommand(object parameters)
         {
-            MessageBoxResult result = MessageBox.Show("You sure'bout that?", "Delete Confirm",MessageBoxButton.YesNo, MessageBoxImage.Question);
-            if(result == MessageBoxResult.Yes)
+            MessageBoxResult result = MessageBox.Show("You sure'bout that?", "Delete Confirm", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (result == MessageBoxResult.Yes)
             {
                 _studentContext.Delete(SelectedItem.StudentCode);
                 Students = _studentContext.GetAll().Select(s => new StudentModel(s)).ToList();
@@ -145,38 +130,6 @@ namespace BHGroup.App.ViewModels
         {
 
         }
-
-        private bool CanExecuteAddStudentCommand(object parameters) {
-            return true;
-        }
-        private void ExecuteAddStudentCommand(object parameters)
-        {
-            var inputFirstName = StudentToAdd.InputFirstName;
-            var inputLastName = StudentToAdd.InputLastName;
-            var inputDOB = StudentToAdd.InputDOB;
-            var inputJoinDate = StudentToAdd.InputJoinDate;
-            var inputGender = StudentToAdd._inputGender;
-            var inputStatus = StudentToAdd.InputStatus;
-
-            if (inputFirstName == null || inputLastName == null || inputDOB == null ||
-                inputJoinDate == null || inputGender == null || inputStatus == null)
-            {
-                MessageBox.Show("Please fill in every required field", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
-            }
-            else
-            {
-                var dob = inputDOB.Split("/").Select(d => int.Parse(d)).ToArray();
-                var joinDate = inputJoinDate.Split("/").Select(d => int.Parse(d)).ToArray();
-                _studentContext.Add(new Student()
-                {
-                    FirstName = inputFirstName,
-                    LastName = inputLastName,
-                    DateOfBirth = new DateTime(dob[2], dob[1], dob[0]),
-                    Gender = inputGender == "Male" ? Person.EGender.Male : Person.EGender.Female,
-                    JoinDate = new DateTime(joinDate[2], joinDate[1], joinDate[0]),
-                    Status = inputStatus == "Active" ? Person.EStatus.Active : Person.EStatus.Inactive,
-                });
-            }
-        }
+        #endregion
     }
 }
---
+++ BHGroup.App/ViewModels/StudentViewModel/StudentAddEditViewModel.cs
@@ -0,0 +1,78 @@
+﻿using BHGroup.App.Models;
+using BHGroup.App.Public.Core;
+using BHGroup.BL;
+using BHGroup.DAL.Entities;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BHGroup.App.ViewModels.StudentViewModel
+{
+    class StudentAddEditViewModel : ObservableObject
+    {
+        #region Data context & repositories
+        private readonly IStudent _studentContext;
+        #endregion
+
+        #region Binding
+        private StudentModel _studentInputObject { get; set; }
+        public StudentModel StudentInputObject
+        {
+            get
+            {
+                return _studentInputObject;
+            }
+            set
+            {
+                _studentInputObject = value; OnPropertyChanged();
+            }
+        }
+        #endregion
+        public RelayCommand AddStudentCommand { get; private set; }
+
+        public StudentAddEditViewModel()
+        {
+            _studentContext = DIHelper.Get().Services.GetRequiredService<IStudent>();
+            StudentInputObject = new StudentModel();
+            AddStudentCommand = new RelayCommand(ExecuteAddStudentCommand, CanExecuteAddStudentCommand);
+        }
+
+        private bool CanExecuteAddStudentCommand(object parameters)
+        {
+            return true;
+        }
+        private void ExecuteAddStudentCommand(object parameters)
+        {
+            var inputFirstName = StudentInputObject.InputFirstName;
+            var inputLastName = StudentInputObject.InputLastName;
+            var inputDOB = StudentInputObject.InputDOB;
+            var inputJoinDate = StudentInputObject.InputJoinDate;
+            var inputGender = StudentInputObject._inputGender;
+            var inputStatus = StudentInputObject.InputStatus;
+
+            if (inputFirstName == null || inputLastName == null || inputDOB == null ||
+                inputJoinDate == null || inputGender == null || inputStatus == null)
+            {
+                MessageBox.Show("Please fill in every required field", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            else
+            {
+                var dob = inputDOB.Split("/").Select(d => int.Parse(d)).ToArray();
+                var joinDate = inputJoinDate.Split("/").Select(d => int.Parse(d)).ToArray();
+                _studentContext.Add(new Student()
+                {
+                    FirstName = inputFirstName,
+                    LastName = inputLastName,
+                    DateOfBirth = new DateTime(dob[2], dob[1], dob[0]),
+                    Gender = inputGender == "Male" ? Person.EGender.Male : Person.EGender.Female,
+                    JoinDate = new DateTime(joinDate[2], joinDate[1], joinDate[0]),
+                    Status = inputStatus == "Active" ? Person.EStatus.Active : Person.EStatus.Inactive,
+                });
+            }
+        }
+    }
+}
